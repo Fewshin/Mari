4cddf3b514f95133362aa31da9f496174d5a20c7
Fewshin <zorkcraft@gmail.com>
2016-10-12 13:13:46 -0500

    Adding logging and allowing for multiple admin IDs.

--- Mari.js
+++ Mari.js
@@ -1,11 +1,14 @@
 var config = require("./config.json")
 var package = require("./package.json")
+const chalk = require('chalk');
 var express = require('express');
 var app = express();
 app.use('/images', express.static(config.imageDirectory))
+var passport = require('passport')
+var OAuth2Strategy = require('passport-oauth').OAuth2Strategy
 var redis = require('redis')
 var client = redis.createClient({detect_buffers: true, db: 15})
-client.on('error', err => {console.log('Redis Error: ' + err)})
+client.on('error', err => {console.log(chalk.bgRed.bold(' Redis Error ') + err)})
 const Eris = require('eris');
 var bot = new Eris.CommandClient(config.token, {}, {
     description: package.description,
@@ -13,27 +16,34 @@ var bot = new Eris.CommandClient(config.token, {}, {
     prefix: config.prefix
 });
 bot.on('ready', () => {
-    console.log('Bot is active');
+    console.log(chalk.bgGreen.bold(' Eris ') + ' Bot is active');
     if (isNaN(config.notificationChannel) === false) {
     bot.createMessage(config.notificationChannel, config.notificationMessage)}
-    console.log('Number of Admins: ' + config.adminids.length)
+    console.log(chalk.bgCyan.bold(' Config ') + ' Number of Admins: ' + config.adminids.length)
     bot.editGame({
         name: config.defaultgame
     })
 })
 //==================================================================
-bot.registerCommand('ping', 'Pong!', { 
+bot.registerCommand('ping', (msg,args) => {
+    console.log(chalk.bgYellow.bold(' Command ') + ' ' + chalk.magenta.bold(msg.channel.guild.name) + ' > ' + chalk.cyan.bold(msg.author.username) + ': ' + msg.content)
+    return 'Pong!'
+}, { 
     description: 'Pong!',
     fullDescription: 'Used to check if the bot is up.'
 });
 //======================================================================
-bot.registerCommand('joke', 'http://i.imgur.com/jlVc2k7.jpg', { 
+bot.registerCommand('joke', (msg,args) => {
+    console.log(chalk.bgYellow.bold(' Command ') + ' ' + chalk.magenta.bold(msg.channel.guild.name) + ' > ' + chalk.cyan.bold(msg.author.username) + ': ' + msg.content)
+    return 'http://i.imgur.com/jlVc2k7.jpg'
+}, { 
     description: 'It\'s Joke',
     fullDescription: 'IT\'s JUST A PRANK BRO'
 });
 //====================================================================
 //todo.push(args[0])
-bot.registerCommand('todo', (msg ,args, todo) =>{
+bot.registerCommand('todo', (msg ,args) =>{
+    console.log(chalk.bgYellow.bold(' Command ') + ' ' + chalk.magenta.bold(msg.channel.guild.name) + ' > ' + chalk.cyan.bold(msg.author.username) + ': ' + msg.content)
     if(config.adminids.indexOf(msg.author.id) > -1){
         client.sadd('todo', args.join(' '))
         return 'Todo Updated!'
@@ -47,18 +57,20 @@ bot.registerCommand('todo', (msg ,args, todo) =>{
 })
 //==================================================================
 bot.registerCommand('echo', (msg ,args) =>{
+    console.log(chalk.bgYellow.bold(' Command ') + ' ' + chalk.magenta.bold(msg.channel.guild.name) + ' > ' + chalk.cyan.bold(msg.author.username) + ': ' + msg.content)
     if(config.adminids.indexOf(msg.author.id) > -1){
         return args.join(' ')
     }
     else{
         return 'You don\'t have permission to do this.'
-    }
+        }
 }, {
     description: 'm!echo',
     fullDescription: 'Echos what you say.'
 })
 //====================================================================================================================================================================================
 bot.registerCommand('stats', (msg ,args) =>{
+    console.log(chalk.bgYellow.bold(' Command ') + ' ' + chalk.magenta.bold(msg.channel.guild.name) + ' > ' + chalk.cyan.bold(msg.author.username) + ': ' + msg.content)
     return '__**Bot Information**__:\n' +
     '\n**Version**: ' +
     package.version +
@@ -86,5 +98,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(config.portListen, function () {
-  console.log('Localhost is active');
+  console.log(chalk.bgMagenta(' Express ') + ' Localhost is active');
 });
