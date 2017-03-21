2ed1fa8de15dcd3bf7e088947229c9ec749c281b
Fewshin <zorkcraft@gmail.com>
2017-03-21 02:09:29 -0500

    Adding support on the llroll command for even numbered scout counts up to 10.

--- commands/basicCommands/llroll.js
+++ commands/basicCommands/llroll.js
@@ -15,8 +15,7 @@ class llroll {
     return null
   }
   command (msg, bot, tag, args) {
-    let RNG = Math.ceil(Math.random() * 100) 
-    function rarityFinder () {
+    function rarityFinder (RNG) {
       if (RNG === 1) {
         return 'UR'
       }
@@ -30,17 +29,52 @@ class llroll {
         return 'R'
       }
     }
-    let searchURL = 'http://schoolido.lu/api/cards/?&is_special=False&is_event=False&ordering=random&is_promo=False&rarity=' + rarityFinder()
-    request
-      .get(searchURL)
-      .end((err, res) => {
-        if (err) { log.error(err) ; bot.createMessage(msg.channel.id, 'There was an error, contact Fewshin#5253 with the details. ```' + err.message + '```') }
-        else {
-          const payload = res.body.results[0]
-          msg.channel.sendTyping().then(render.loveLiveCards([`http:${payload.card_image}`], function (idolImage) {bot.createMessage(msg.channel.id, `${rarityFinder()}\n${payload.translated_collection} ${payload.idol.name}\nThis command is in testing, if there are issues don\'t hesitate to contact Fewshin#5253 with details.`, { file: idolImage, name: `${payload.translated_collection}_${payload.idol.name}.png` })}))
-        }
-      })
+    function collectionChecker (payload) {
+      if (payload.translated_collection === null) { return 'Rare' }
+      else { return payload.translated_collection }
+    }
+    function messageMaker (idolName, rCount, srCount, ssrCount, urCount, cardCount, payload) {
+      if (cardCount > 1) {
+        let countArray = [`URs: ${urCount.length}`, `SSRs: ${ssrCount.length}`, `SRs: ${srCount.length}`, `Rs: ${rCount.length}`]
+        return `${countArray.join('; ')}\n${idolName.join(', ')}`
+      }
+      else { return `${payload.rarity}\n${collectionChecker(payload)} ${payload.idol.name}` }
+    }
+    if (args[0] === null || args[0] === undefined) { args[0] = 1 }
+    if (args[0] % 1 === 0 && args[0] < 12) {
+      let urlArray = []
+      let idolName = []
+      let rCount = []
+      let srCount = []
+      let ssrCount = []
+      let urCount = []
+      for (let i = 0; i < args[0]; i++) {
+        let RNG = Math.ceil(Math.random() * 100) 
+        let searchURL = 'http://schoolido.lu/api/cards/?&is_special=False&is_event=False&ordering=random&is_promo=False&rarity=' + rarityFinder(RNG)
+        request
+          .get(searchURL)
+          .end((err, res) => {
+            if (err) { log.error(err) ; bot.createMessage(msg.channel.id, 'There was an error, contact Fewshin#5253 with the details. ```' + err.message + '```') }
+            else {
+              const payload = res.body.results[0]
+              idolName.push(`${collectionChecker(payload)} ${payload.idol.name}`)
+              urlArray.push(`http:${payload.card_image}`)
+              if (rarityFinder(RNG) === 'R') { rCount.push('R') }
+              else if (rarityFinder(RNG) === 'SR') { srCount.push('SR') }
+              else if (rarityFinder(RNG) === 'SSR') { ssrCount.push('SSR') }
+              else if (rarityFinder(RNG) === 'UR') { urCount.push('UR') }
+              else { return }
+              if (urlArray.length == args[0]) {
+                log.custom('bgCyan', 'urlArray',`[${urlArray}]`)
+                msg.channel.sendTyping().then(render.loveLiveCards(urlArray, function (idolImage) {
+                  bot.createMessage(msg.channel.id, messageMaker(idolName, rCount, srCount, ssrCount, urCount, args[0], payload), { file: idolImage, name: `${payload.translated_collection}_${payload.idol.name}.png` }) 
+                })) 
+              } 
+            }
+          })
+      }
+    }
   }  
 }
-//bot.createMessage(msg.channel.id, `${rarityFinder()}\n${payload.translated_collection} ${payload.idol.name}\nThis command is in testing, if there are issues don\'t hesitate to contact Fewshin#5253 with details.`, { file: render.loveLiveCards([`http:${payload.card_image}`]), name: `${payload.translated_collection}_${payload.idol.name}.png` })
+
 module.exports = llroll
--- utilities/canvasManager.js
+++ utilities/canvasManager.js
@@ -19,6 +19,31 @@ class canvasManager {
           return callback(res.body)
         })
     }
+    else if (urlArray.length % 2 === 0 && urlArray.length < 12) {
+      let canvas = new Canvas((urlArray.length / 2) * 512 , 1440)
+      let ctx = canvas.getContext('2d')
+      let counter = 0
+      for (let i = 0; i < urlArray.length; i++) {
+        request
+          .get(urlArray[i])
+          .end(function(err, res){
+            if (err) { log.error(err) }
+            let img = new Image()
+            function heightBoiz () {
+              if (i % 2 === 0) { return 0 }
+              else { return 720 }
+            }
+            img.onload = function () {
+              ctx.drawImage(img, Math.floor(i/2)* 512,(i%2) *720, 512, 720)
+              log.custom('bgCyan', 'Canvas', `${i} Love Live card rendered`)
+              counter++
+              if (counter === urlArray.length) { return callback(canvas.toBuffer(img)) }
+            }
+            img.onerror = function (err) { log.error(err) }
+            img.src = res.body
+          })  
+      }
+    }
   }
 }
 
