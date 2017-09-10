b14486d0d2016e3144552bfd369dc8ec4d1c1a32
Fewshin <zorkcraft@gmail.com>
2017-09-10 16:55:39 -0500

    Making the sharding more shard like, removing nodemon, and setting up shop for the reload command.

--- Mari.js
+++ Mari.js
@@ -1,3 +1,16 @@
+const cluster = require('cluster')
+
 const Bot = require('./core')
 const Mari = new Bot() 
-Mari.botOn()
+const Logger = require('./utilities/Logger')
+const log = new Logger('bgBlue', 'System')
+const config = require('./config.json')
+
+if (cluster.isMaster){
+    for (let i = 0; i < config.shardCount; i++) {
+        cluster.fork({ 'SHARD_ID' : i, 'SHARD_COUNT' : config.shardCount })
+    }
+}
+else {
+    Mari.botOn()
+}
--- core.js
+++ core.js
@@ -6,21 +6,21 @@ const chron = new chronManager()
 const commandFrame = require('./commands/commandManager')
 const input = new commandFrame()
 const Eris = require('eris')
-log.system(`Initiating`)
-
 class Core {
   constructor (options) {
     this.options = options || {}
-    this.shardCount = config.shardCount - 1 + 1
+    this.shardCount = process.env.SHARD_COUNT % 10
+    this.shardID = process.env.SHARD_ID % 10
     this.imageSize = config.defaultImageSize
     this.imageFormat = config.defaultImageFormat
     this.token = config.token
   }
   botOn () {
     if (this.token === null || this.token === undefined) { log.error(`No Token`) }
-    this.bot = new Eris.Client(this.token, {autoreconnect: true, maxShards: this.shardCount, defaultImageSize: this.imageSize, defaultImageFormat: this.imageFormat})
+    this.bot = new Eris.Client(this.token, {autoreconnect: true, maxShards: this.shardCount, firstShardID: this.shardID, lastShardID: this.shardID, defaultImageSize: this.imageSize, defaultImageFormat: this.imageFormat})
+    log.system(`Initiating shard ${this.shardID}`)
     this.bot.on('shardReady', id => { log.system(`Shard ${id} is active.`) })
-    this.bot.on('ready', () => { log.system(`Bot is on`) })
+    //this.bot.on('ready', () => { log.system(`Bot is on`) })
     this.bot.on('error', err => { log.error(`${err}`) ; if (!err) { return } })
     this.bot.on('messageCreate', message => { input.handler(message, this.bot)})
     this.bot.connect()
